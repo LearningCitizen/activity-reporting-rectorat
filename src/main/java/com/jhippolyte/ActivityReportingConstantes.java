bee08950398112b18c67dfb61a128b52fa73844f
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2021-12-06 21:55:05 +0100

    update services

--- src/main/java/com/jhippolyte/ActivityReportingConstantes.java
+++ src/main/java/com/jhippolyte/ActivityReportingConstantes.java
@@ -6,8 +6,17 @@ public final class ActivityReportingConstantes {
     public final static String MERGE_REQUESTS_URI = "/merge_requests";
     public final static String STATE_PARAM = "state";
     public final static String OPENED_STATE = "opened";
-    public final static String OPENED_STATE_FILTER = STATE_PARAM+"="+OPENED_STATE;
+    public final static String MERGED_STATE = "merged";
+    public final static String MERGED_STATE_FILTER = STATE_PARAM + "=" + MERGED_STATE;
     public final static String PRIVATE_TOKEN_HEADER = "PRIVATE-TOKEN";
+    public final static String AND = "&";
+    public final static String CREATED_AFTER = "created_after";
+    public final static String AUTHOR_USERNAME = "author_username";
+    public final static String CREATED_AFTER_FILTER = CREATED_AFTER + "=%s";
+    public final static String AUTHOR_USERNAME_FILTER = AUTHOR_USERNAME + "=%s";
+    public final static String PARAM_OP = "?";
+    public final static String GROUP_URI = "/groups";
+    public final static String GROUP_PATH = "/groups/%s";
 
     private ActivityReportingConstantes() {
     }
--- src/main/java/com/jhippolyte/ActivityReportingService.java
+++ src/main/java/com/jhippolyte/ActivityReportingService.java
@@ -12,8 +12,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 
+import static com.jhippolyte.ActivityReportingConstantes.AND;
+import static com.jhippolyte.ActivityReportingConstantes.AUTHOR_USERNAME_FILTER;
+import static com.jhippolyte.ActivityReportingConstantes.CREATED_AFTER_FILTER;
+import static com.jhippolyte.ActivityReportingConstantes.GROUP_PATH;
+import static com.jhippolyte.ActivityReportingConstantes.MERGED_STATE_FILTER;
 import static com.jhippolyte.ActivityReportingConstantes.MERGE_REQUESTS_URI;
-import static com.jhippolyte.ActivityReportingConstantes.OPENED_STATE_FILTER;
+import static com.jhippolyte.ActivityReportingConstantes.PARAM_OP;
 import static com.jhippolyte.ActivityReportingConstantes.PRIVATE_TOKEN_HEADER;
 
 @Service
@@ -22,12 +27,24 @@ public class ActivityReportingService {
     Logger logger = LoggerFactory.getLogger(ActivityReportingService.class);
     HttpClient httpClient = HttpClient.newHttpClient();
 
-    public List<String> getAllMergeRequestsByUserInGroup(String user, String privateToken, String gitlabGroup, String gitLabUrl) {
+    /**
+     * Get All the merged merge requests from author created after a date.
+     * <p>
+     * Example : GET https://gitlab.com/api/v4/groups/9970/merge_requests?state=merged&created_after=2021-12-01T00:00:00Z&author_username=afontaine
+     *
+     * @param user
+     * @param privateToken
+     * @param gitlabGroup
+     * @param gitLabUrl
+     * @param startDate
+     * @return
+     */
+    public List<String> getAllMergeRequestsByUserInGroup(String user, String privateToken, String gitlabGroup, String gitLabUrl, String startDate) {
         try {
-            logger.info("Building the request to get merge request - user "+user+" group "+gitlabGroup+" url "+gitLabUrl);
+            logger.info("Building the request to get merge request - user " + user + " group " + gitlabGroup + " url " + gitLabUrl);
             HttpRequest getMRrequest = HttpRequest.newBuilder()
-                    .uri(new URI(gitLabUrl+MERGE_REQUESTS_URI+"?"+OPENED_STATE_FILTER) )
-                    .headers(PRIVATE_TOKEN_HEADER , privateToken)
+                    .uri(new URI(gitLabUrl + String.format(GROUP_PATH, gitlabGroup) + MERGE_REQUESTS_URI + PARAM_OP + MERGED_STATE_FILTER + AND + String.format(AUTHOR_USERNAME_FILTER, user) + AND + String.format(CREATED_AFTER_FILTER, startDate)))
+                    .headers(PRIVATE_TOKEN_HEADER, privateToken)
                     .GET()
                     .build();
             HttpResponse<String> response = httpClient.send(getMRrequest, HttpResponse.BodyHandlers.ofString());
