012989ef034cc780c2793a910309680d528a926a
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2021-12-06 21:56:51 +0100

    terminate application + update service call

--- src/main/java/com/jhippolyte/ActivityReportingApplication.java
+++ src/main/java/com/jhippolyte/ActivityReportingApplication.java
@@ -6,32 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import static com.jhippolyte.ActivityReportingConstantes.DIAPASON_GITLAB_GROUP;
-import static com.jhippolyte.ActivityReportingConstantes.DIAPASON_GITLAB_URL;
 
 @SpringBootApplication
 public class ActivityReportingApplication {
 
     Logger LOGGER = LoggerFactory.getLogger(ActivityReportingApplication.class);
-    private String gitlabUser = "hjohnson";
-    private String privateToken = "";
-    private String gitlabGroup = DIAPASON_GITLAB_GROUP;
-    private String gitLabUrl = ActivityReportingConstantes.DIAPASON_GITLAB_URL;
-
     @Autowired
     ActivityReportingService activityReportingService;
+    private String gitlabUser = "aUser";
+    private String privateToken = "myPrivateToken";
+    private String gitlabGroup = "9970";
+    private String gitLabUrl = "https://gitlab.com/api/v4";
+    private String startDate = "2021-12-01T00:00:00Z";
 
     public static void main(String[] args) {
-        SpringApplication.run(ActivityReportingApplication.class);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(ActivityReportingApplication.class);
+        configurableApplicationContext.close();
     }
 
     @Bean
-    CommandLineRunner run (){
+    CommandLineRunner run() {
         return (args) -> {
             LOGGER.info("Activity Reporting started");
-            activityReportingService.getAllMergeRequestsByUserInGroup(gitlabUser, privateToken, gitlabGroup, gitLabUrl);
+            activityReportingService.getAllMergeRequestsByUserInGroup(gitlabUser, privateToken, gitlabGroup, gitLabUrl, startDate);
         };
     }
 
